cb780b0c3ac1373a50de8553af89f04bf02b682a
Kirk Morris <worker.kirk@gmail.com>
2023-06-05 20:38:07 -0700

    Switches hold-to-talk to tap-to-talk, tap-to-stop.

--- src/components/MessageBar.tsx
+++ src/components/MessageBar.tsx
@@ -3,7 +3,7 @@ import { useEffect, useRef, useState } from "react";
 import TextareaAutosize from 'react-textarea-autosize';
 import { createSpeechlySpeechRecognition } from '@speechly/speech-recognition-polyfill';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-import { Mic, Send } from "react-feather";
+import { Mic, Send, Square } from "react-feather";
 import config from '../../jabberbase.config.json';
 
 const appId = config.speechlyAppId;
@@ -19,12 +19,10 @@ interface MessageBarProps {
 export function MessageBar({ msg, onChange, onSend}: MessageBarProps) {
   const doAudio = (msg.trim().length === 0);
   const inputRef = useRef<HTMLTextAreaElement>(null);
-  const [focused, setFocused] = useState(false);
   const [recording, setRecording] = useState(false);
   // const [audioFile, setAudioFile] = useState<any>(null);
   const {
     transcript,
-    resetTranscript
   } = useSpeechRecognition();
 
   useEffect(() => {
@@ -32,35 +30,22 @@ export function MessageBar({ msg, onChange, onSend}: MessageBarProps) {
   }, [transcript]);
 
   function handleUp() {
-    if (!recording) {
-      const audioFile = null;
-      onSend(audioFile);
+    if (recording) {
+      SpeechRecognition.stopListening();
+      setRecording(false);
     }
     else {
-      SpeechRecognition.stopListening();
-    }
-
-    if (focused) {
-      inputRef.current?.focus();
-    }
-
-    setRecording(false);
-    resetTranscript();
-  }
-
-  function handleDown() {
-    if (doAudio) {
-      setRecording(true);
-      SpeechRecognition.startListening();
-    }
-  }
-
-  function delayedBlur() {
-    setTimeout(() => {
-      if (document.activeElement !== inputRef.current) {
-        setFocused(false);
+      if (doAudio) {
+        setRecording(true);
+        SpeechRecognition.startListening();
       }
-    }, 100);
+      else {
+        const audioFile = null;
+        onSend(audioFile);
+      }
+    }
+
+    // inputRef.current?.focus();
   }
 
   function ucFirst(str: string) {
@@ -68,28 +53,28 @@ export function MessageBar({ msg, onChange, onSend}: MessageBarProps) {
   }
   
   return (
-    <div className={`z-40 fixed h-20 bottom-0 left-0 w-full p-2 flex flex-row items-end bg-gradient-to-t from-white to-white/0`}>
+    <div
+      className={`z-40 fixed bottom-0 left-0 w-full p-2 flex flex-row items-end bg-gradient-to-t from-white to-white/0`}
+    >
       <TextareaAutosize
         className={`min-h-12 border-2 p-2 rounded-md flex-1 mr-2 outline-none resize-none transition-all ${recording ? 'border-red-500 bg-red-500 animate-pulse' : 'border-black'}`}
         value={msg}
         onChange={(e) => onChange(e.target.value)}
         maxRows={4}
         ref={inputRef}
-        onFocus={() => setFocused(true)}
-        onBlur={delayedBlur}
         disabled={recording}
       />
 
       <button
-        className={`w-[44px] h-[44px] text-white rounded-md flex items-center justify-center overflow-hidden transition-all ${recording ? 'bg-red-500 animate-pulse scale-150' : 'bg-black'}`}
-        onPointerDown={handleDown}
-        onPointerUp={handleUp}
-        onPointerCancel={handleUp}
+        className={`w-[44px] h-[44px] text-white rounded-md flex items-center justify-center overflow-hidden transition-all ${recording ? 'bg-red-500 animate-pulse' : 'bg-black'}`}
+        onClick={handleUp}
       >
         {
-          doAudio
-          ? <Mic size={22} />
-          : <Send size={22} />
+          recording
+          ? <Square fill='#FFF' size={22} />
+          : doAudio
+            ? <Mic size={22} />
+            : <Send size={22} />
         }
       </button>
     </div>
